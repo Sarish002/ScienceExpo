7dd3d94dfb49910fb90bcc75fa6155fa44e4f434
Abhinav. S. Nair <165151802+Sarish002@users.noreply.github.com>
2025-08-28 21:11:18 +0530

    Update OrgAI.py

--- Python/OrgAI.py
+++ Python/OrgAI.py
@@ -1,1 +1,14 @@
+import NomialAnalysis
+import FormulaicAnalysis
+
+# Logic
+while True:
+    query = input("Query: ")
+    for i in range(10):
+        if str(i) in query:
+            FormulaicAnalysis.FormulaicAnalysis(query)
+            break
+    else:
+        NomialAnalysis.NomialAnalysis(query)
+    print("---------------")
 
