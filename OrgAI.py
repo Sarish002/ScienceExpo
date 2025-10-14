35a3ecf3b81da341905fc543741c970c3216b8aa
Abhinav. S. Nair <165151802+Sarish002@users.noreply.github.com>
2025-10-14 07:12:50 +0530

    Add files via upload

---
+++ OrgAI.py
@@ -0,0 +1,22 @@
+import sys
+import NomialAnalysis
+import FormulaicAnalysis
+
+# Logic
+def Logic(query):
+    if query == "Q":
+        sys.exit()
+    try:
+        for i in range(10):
+            if str(i) in query or query in ["HCHO", "HCOOH"]:
+                return FormulaicAnalysis.FormulaicAnalysis(query.upper())
+                break
+        else:
+            return NomialAnalysis.NomialAnalysis(query.title())
+    except:
+        return "Couldn't find an answer."
+
+if __name__ == "__main__":
+    while True:
+        query = input("Query (or) Quit (Q): ")
+        print(Logic(query))
